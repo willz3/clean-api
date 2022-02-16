c33aa9f4c52ec660602ad4b9bf554009e9c5f192
willian.vassilek <willz3@outlook.com>
2022-02-16 10:10:23 -0300

    refactor: simplify field validate code in SignUpController.handle

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -4,12 +4,12 @@ import { badRequest } from "../helpers/httpHelper";
 
 export class SignUpController {
 	handle(httpRquest: HttpRequest): HttpResponse {
-		if (!httpRquest.body.name) {
-			return badRequest(new MissingParamError("name"));
-		}
+		const requiredFields = ["name", "email"];
 
-		if (!httpRquest.body.email) {
-			return badRequest(new MissingParamError("email"));
+		for (const field of requiredFields) {
+			if (!httpRquest.body[field]) {
+				return badRequest(new MissingParamError(field));
+			}
 		}
 	}
 }
