aca80b6d41e7d694f83f16af5cee37eb589fffb1
willian.vassilek <willz3@outlook.com>
2022-02-21 15:15:38 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -227,4 +227,25 @@ describe("SignUp Controller request fields", () => {
 		expect(httpResponse.statusCode).toBe(500);
 		expect(httpResponse.body).toEqual(new ServerError());
 	});
+
+	test("Should return 200 if valid data is provided", () => {
+		const { sut, emailValidatorStub } = makeSut();
+
+		const httpRequest = {
+			body: {
+				name: "valid_name",
+				email: "valid_email@mail.com",
+				password: "valid_password",
+				password_confirmation: "valid_password",
+			},
+		};
+		const httpResponse = sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(200);
+		expect(httpResponse.body).toEqual({
+			id: "valid_id",
+			name: "valid_name",
+			email: "valid_email@mail.com",
+			password: "valid_password",
+		});
+	});
 });
