a89b326d2ec686fba549bd8e5383201d3a7e44f3
willian.vassilek <willz3@outlook.com>
2022-02-21 15:17:44 -0300

    refactor: move success http response to a helper method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -6,7 +6,7 @@ import {
 	AddAccount,
 } from "./signup-protocols";
 import { MissingParamError, InvalidParamError } from "../../errors";
-import { badRequest, serverError } from "../../helpers/http-helper";
+import { badRequest, ok, serverError } from "../../helpers/http-helper";
 
 export class SignUpController implements Controller {
 	private readonly emailValidator: EmailValidator;
@@ -49,10 +49,7 @@ export class SignUpController implements Controller {
 				password,
 			});
 
-			return {
-				statusCode: 200,
-				body: account,
-			};
+			return ok(account);
 		} catch (error) {
 			return serverError();
 		}
