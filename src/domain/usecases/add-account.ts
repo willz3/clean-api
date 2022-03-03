c4926bbcf4f982e4c20720a3be924f267675fb59
willian.vassilek <willz3@outlook.com>
2022-03-02 21:22:15 -0300

    feat: ensure DbAddAccount calls encrypter with correct password

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -7,5 +7,5 @@ export interface AddAccountModel {
 }
 
 export interface AddAccount {
-	add(account: AddAccountModel): Promise<AddAccountModel>;
+	add(account: AddAccountModel): Promise<AccountModel>;
 }
