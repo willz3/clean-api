79458dc4d373ee4453e48efbc8315fd4f20dd2f1
willian.vassilek <willz3@outlook.com>
2022-06-14 05:55:05 -0300

    test: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,7 @@
 import { InvalidParamError, MissingParamError } from "../../errors";
 import {
 	badRequest,
+	ok,
 	serverError,
 	unauthorized,
 } from "../../helpers/http-helper";
@@ -133,5 +134,16 @@ describe("Login Controller", () => {
 
 		const httpResponse = await sut.handle(makeFakeRequest());
 		expect(httpResponse).toEqual(serverError(new Error()));
+	});
+
+	it("Should return 200 if valid credentials are provided", async () => {
+		const { sut } = makeSut();
+
+		const httpResponse = await sut.handle(makeFakeRequest());
+		expect(httpResponse).toEqual(
+			ok({
+				accessToken: "any_token",
+			})
+		);
 	});
 });
