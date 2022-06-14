fcaf4c2216119b05f184cf62ac3fdb262ca667fc
willian.vassilek <willz3@outlook.com>
2022-06-14 05:30:45 -0300

    test: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,3 +1,4 @@
+import { Authentication } from "../../../domain/usecases/authentication";
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { badRequest, serverError } from "../../helpers/http-helper";
 import { HttpRequest } from "../../protocols";
@@ -14,6 +15,16 @@ const makeEmailValidator = (): EmailValidator => {
 	return new EmailValidatorStub();
 };
 
+const makeAuthentication = (): Authentication => {
+	class AuthenticationStub implements Authentication {
+		async auth(email: string, password: string): Promise<string> {
+			return Promise.resolve("any_token");
+		}
+	}
+
+	return new AuthenticationStub();
+};
+
 const makeFakeRequest = (): HttpRequest => {
 	return {
 		body: {
@@ -26,13 +37,15 @@ const makeFakeRequest = (): HttpRequest => {
 interface SutTypes {
 	sut: LoginController;
 	emailValidatorStub: EmailValidator;
+	authenticationStub: Authentication;
 }
 
 const makeSut = (): SutTypes => {
 	const emailValidatorStub = makeEmailValidator();
-	const sut = new LoginController(emailValidatorStub);
+	const authenticationStub = makeAuthentication();
+	const sut = new LoginController(emailValidatorStub, authenticationStub);
 
-	return { sut, emailValidatorStub };
+	return { sut, emailValidatorStub, authenticationStub };
 };
 
 describe("Login Controller", () => {
@@ -87,5 +100,14 @@ describe("Login Controller", () => {
 
 		const httpResponse = await sut.handle(makeFakeRequest());
 		expect(httpResponse).toEqual(serverError(new Error()));
+	});
+
+	it("Should call Authentication with correct values", async () => {
+		const { sut, authenticationStub } = makeSut();
+
+		const authSpy = jest.spyOn(authenticationStub, "auth");
+
+		await sut.handle(makeFakeRequest());
+		expect(authSpy).toHaveBeenCalledWith("any_email@mail.com", "any_password");
 	});
 });
