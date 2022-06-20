f64536ca9371eb85861b3481edb43e4c0d0752f1
willian.vassilek <willz3@outlook.com>
2022-06-20 05:38:52 -0300

    test: ensure LoginController returns 400 if Validation fails

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -172,4 +172,13 @@ describe("Login Controller", () => {
 		await sut.handle(httpRequest);
 		expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
 	});
+
+	it("Should return 400 if Validation returns an error", async () => {
+		const { sut, validationStub } = makeSut();
+
+		jest.spyOn(validationStub, "validate").mockReturnValueOnce(new Error());
+
+		const httpResponse = await sut.handle(makeFakeRequest());
+		expect(httpResponse).toEqual(badRequest(new Error()));
+	});
 });
