d701a1ee104ed031cd5ea5c82b3efdf54143524f
willian.vassilek <willz3@outlook.com>
2022-08-03 20:26:26 -0300

    feat: rename Encrypter to Hasher

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,23 +1,23 @@
 import {
 	AccountModel,
 	AddAccountModel,
-	Encrypter,
+	Hasher,
 	AddAccountRepository,
 } from "./db-add-account-protocols";
 import { DbAddAccount } from "./db-add-account";
 
 interface SutTypes {
 	sut: DbAddAccount;
-	encrypterStub: Encrypter;
+	hasherStub: Hasher;
 	addAccountRepositoryStub: AddAccountRepository;
 }
 
 const makeSut = (): SutTypes => {
-	const encrypterStub = makeEncrypter();
+	const hasherStub = makeHasher();
 	const addAccountRepositoryStub = makeAddAccountRepository();
-	const sut = new DbAddAccount(encrypterStub, addAccountRepositoryStub);
+	const sut = new DbAddAccount(hasherStub, addAccountRepositoryStub);
 
-	return { sut, encrypterStub, addAccountRepositoryStub };
+	return { sut, hasherStub, addAccountRepositoryStub };
 };
 
 const makeFakeAccount = (): AccountModel => {
@@ -47,28 +47,28 @@ const makeAddAccountRepository = (): AddAccountRepository => {
 	return new AddAccountRepositoryStub();
 };
 
-const makeEncrypter = (): Encrypter => {
-	class EncrypterStub implements Encrypter {
-		async encrypt(value: string): Promise<string> {
+const makeHasher = (): Hasher => {
+	class HasherStub implements Hasher {
+		async hash(value: string): Promise<string> {
 			return new Promise<string>((resolve) => resolve("hashed_password"));
 		}
 	}
 
-	return new EncrypterStub();
+	return new HasherStub();
 };
 
 describe("DbAddAccount Usecase", () => {
-	test("Should call Encrypter with correct  password", async () => {
-		const { sut, encrypterStub } = makeSut();
-		const encryptSpy = jest.spyOn(encrypterStub, "encrypt");
+	test("Should call Hasher with correct  password", async () => {
+		const { sut, hasherStub } = makeSut();
+		const hashSpy = jest.spyOn(hasherStub, "hash");
 		await sut.add(makeFakeAccountData());
-		expect(encryptSpy).toHaveBeenCalledWith("valid_password");
+		expect(hashSpy).toHaveBeenCalledWith("valid_password");
 	});
 
-	test("Should throw if encrypter throws", async () => {
-		const { sut, encrypterStub } = makeSut();
+	test("Should throw if hasher throws", async () => {
+		const { sut, hasherStub } = makeSut();
 		jest
-			.spyOn(encrypterStub, "encrypt")
+			.spyOn(hasherStub, "hash")
 			.mockReturnValueOnce(
 				new Promise((resolve, reject) => reject(new Error()))
 			);
--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -18,13 +18,13 @@ describe("BCrypt Adapter", () => {
 	test("Should call bcrypt with correct values", async () => {
 		const sut = makeSut();
 		const hashSpy = jest.spyOn(bcrypt, "hash");
-		await sut.encrypt("any_value");
+		await sut.hash("any_value");
 		expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
 	});
 
 	test("Should return a hash on success", async () => {
 		const sut = makeSut();
-		const hash = await sut.encrypt("any_value");
+		const hash = await sut.hash("any_value");
 
 		expect(hash).toBe("hash");
 	});
