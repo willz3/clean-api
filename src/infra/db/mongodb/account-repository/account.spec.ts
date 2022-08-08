af0c9f9be545b1331e4a4831416642ff3df06aa2
willian.vassilek <willz3@outlook.com>
2022-08-08 20:34:57 -0300

    test: ensure AccountRepository updates the account accessToken on UpdateAccesToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -61,4 +61,21 @@ describe("Account Mongo Repository", () => {
 
 		expect(account).toBeFalsy();
 	});
+
+	test("Should update the account accessToken on UpdateAccesToken success", async () => {
+		const result = await accountCollection.insertOne({
+			name: "any_name",
+			email: "any_email@mail.com",
+			password: "any_password",
+		});
+		const accountResult = result.ops[0];
+		expect(accountResult.accessToken).toBeFalsy();
+
+		const sut = makeSut();
+		await sut.updateAccessToken(accountResult._id, "any_token");
+		const account = await accountCollection.findOne({ _id: accountResult._id });
+
+		expect(account).toBeTruthy();
+		expect(account.accessToken).toBe("any_token");
+	});
 });
