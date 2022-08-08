9618acf9da74085df2e6d9f5b1992e0c8981cf2a
willian.vassilek <willz3@outlook.com>
2022-08-08 20:25:47 -0300

    test: ensure AccountRepository returns  an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,6 +1,8 @@
+import { Collection } from "mongodb";
 import { MongoHelper } from "../helpers/mongo-helper";
 import { AccountMongoRepository } from "./account";
 
+let accountCollection: Collection;
 describe("Account Mongo Repository", () => {
 	beforeAll(async () => {
 		await MongoHelper.connect(process.env.MONGO_URL);
@@ -10,8 +12,8 @@ describe("Account Mongo Repository", () => {
 		await MongoHelper.disconnect();
 	});
 
-	beforeEach(() => async () => {
-		const accountCollection = await MongoHelper.getCollection("accounts");
+	beforeEach(async () => {
+		accountCollection = await MongoHelper.getCollection("accounts");
 		await accountCollection.deleteMany({});
 	});
 
@@ -19,7 +21,7 @@ describe("Account Mongo Repository", () => {
 		return new AccountMongoRepository();
 	};
 
-	test("Should return an account on success", async () => {
+	test("Should return an account on add success", async () => {
 		const sut = makeSut();
 		const account = await sut.add({
 			name: "any_name",
@@ -29,6 +31,24 @@ describe("Account Mongo Repository", () => {
 
 		expect(account).toBeTruthy();
 		expect(account.id).toBeTruthy();
+		expect(account.name).toBe("any_name");
+		expect(account.email).toBe("any_email@mail.com");
+		expect(account.password).toBe("any_password");
+	});
+
+	test("Should return an account on loadByEmail success", async () => {
+		await accountCollection.insertOne({
+			name: "any_name",
+			email: "any_email@mail.com",
+			password: "any_password",
+		});
+
+		const sut = makeSut();
+
+		const account = await sut.loadByEmail("any_email@mail.com");
+
+		expect(account).toBeTruthy();
+
 		expect(account.name).toBe("any_name");
 		expect(account.email).toBe("any_email@mail.com");
 		expect(account.password).toBe("any_password");
