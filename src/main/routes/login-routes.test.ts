346a7bbef05c2d1f59a18d321153c674b253dc56
willian.vassilek <willz3@outlook.com>
2022-08-10 18:25:57 -0300

    test: add login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,10 +1,14 @@
 import request from "supertest";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
+import { hash } from "bcrypt";
+import { Collection } from "mongodb";
 
+let accountCollection: Collection;
 describe("Login routes", () => {
 	beforeAll(async () => {
 		await MongoHelper.connect(process.env.MONGO_URL);
+		accountCollection = await MongoHelper.getCollection("accounts");
 	});
 
 	afterAll(async () => {
@@ -12,7 +16,6 @@ describe("Login routes", () => {
 	});
 
 	beforeEach(() => async () => {
-		const accountCollection = await MongoHelper.getCollection("accounts");
 		await accountCollection.deleteMany({});
 	});
 
@@ -25,6 +28,26 @@ describe("Login routes", () => {
 					email: "valid_mail@mail.com",
 					password: "valid_password",
 					passwordConfirmation: "valid_password",
+				})
+				.expect(200);
+		});
+	});
+
+	describe("POST /login", () => {
+		test("Should return 200 on login", async () => {
+			const hashPassword = await hash("valid_password", 12);
+
+			await accountCollection.insertOne({
+				name: "valid_name",
+				email: "valid_mail@mail.com",
+				password: hashPassword,
+			});
+
+			await request(app)
+				.post("/api/login")
+				.send({
+					email: "valid_mail@mail.com",
+					password: "valid_password",
 				})
 				.expect(200);
 		});
