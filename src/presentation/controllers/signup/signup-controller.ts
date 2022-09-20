aac6deee2414496ca053472b45ab8dfe408bf387
willian.vassilek <willz3@outlook.com>
2022-09-20 06:36:56 -0300

    feat: ensure SignUpController returns 403 if account already in use

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -5,8 +5,9 @@ import {
 	AddAccount,
 	Authentication
 } from './signup-controller-protocols';
-import { badRequest, ok, serverError } from '../../helpers/http/http-helper';
+import { badRequest, forbidden, ok, serverError } from '../../helpers/http/http-helper';
 import { Validation } from '../../protocols/validation';
+import { EmailInUseError } from '../../errors';
 
 export class SignUpController implements Controller {
 	constructor(
@@ -25,11 +26,15 @@ export class SignUpController implements Controller {
 
 			const { name, email, password } = httpRequest.body;
 
-			await this.addAccount.add({
+			const account = await this.addAccount.add({
 				name,
 				email,
 				password
 			});
+
+			if (!account) {
+				return forbidden(new EmailInUseError());
+			}
 
 			const accessToken = await this.authentication.auth({ email, password });
 
