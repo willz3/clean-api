5e7c2340ab5e86e0f79cd0084920672e9c0a75b5
Willian Vassilek <willz3@outlook.com>
2024-02-28 14:02:04 -0300

    feat: ensure AddSurveyController returns no content on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest, serverError } from '../../../helpers/http/http-helper';
+import { badRequest, noContent, serverError } from '../../../helpers/http/http-helper';
 import {
 	AddSurvey,
 	Controller,
@@ -27,7 +27,7 @@ export class AddSurveyController implements Controller {
 				answers
 			});
 
-			return null;
+			return noContent();
 		} catch (error) {
 			return serverError(error);
 		}
