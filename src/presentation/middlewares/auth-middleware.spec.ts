a36b947796e0a4021fa99825413cdaa23f9c3deb
Willian Vassilek <willz3@outlook.com>
2024-02-29 15:48:44 -0300

    test: ensure AuthMiddleware returns server error if loadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,4 +1,4 @@
-import { ok, forbidden } from '../helpers/http/http-helper';
+import { ok, forbidden, serverError } from '../helpers/http/http-helper';
 import { AccessDeniedError } from '../errors';
 import { AuthMiddleware } from './auth-middleware';
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token';
@@ -35,6 +35,16 @@ describe('Auth middleware', () => {
 		const httpResponse = await sut.handle(makeFakeRequest());
 
 		expect(httpResponse).toEqual(ok({ accountId: 'valid_id' }));
+	});
+
+	it('Should return server error if loadAccountByToken throws', async () => {
+		const { sut, loadAccountByTokenStub } = makeSut();
+		jest
+			.spyOn(loadAccountByTokenStub, 'load')
+			.mockReturnValueOnce(Promise.reject(new Error()));
+		const httpResponse = await sut.handle(makeFakeRequest());
+
+		expect(httpResponse).toEqual(serverError(new Error()));
 	});
 });
 
