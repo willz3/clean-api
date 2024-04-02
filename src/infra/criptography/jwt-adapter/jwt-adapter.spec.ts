1347271208f7ee952bc34739b8447373f0eab4b1
Willian Vassilek <willz3@outlook.com>
2024-04-02 14:54:09 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -46,5 +46,11 @@ describe('Jwt Adapter', () => {
 			await sut.decrypt('any_token');
 			expect(signSpy).toHaveBeenCalledWith('any_token', 'secret');
 		});
+
+		test('Should return a value on verify success', async () => {
+			const sut = makeSut();
+			const accessToken = await sut.decrypt('any_token');
+			expect(accessToken).toBe('any_value');
+		});
 	});
 });
