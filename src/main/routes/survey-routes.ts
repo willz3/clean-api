9f52139df35d15700901924eac4958eb1d666244
Willian Vassilek <willz3@outlook.com>
2024-04-02 15:47:04 -0300

    feat: ensure post surveys returns 403 on add survey without accesToken

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { adapterRoute } from '../adapters/express/express-routes-adapter';
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory';
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware';
+import { adapterMiddleware } from '../adapters/express/express-middleware-adapter';
 
 export default (router: Router) => {
-	router.post('/surveys', adapterRoute(makeAddSurveyController()));
+	const adminAuth = adapterMiddleware(makeAuthMiddleware('admin'));
+	router.post('/surveys', adminAuth, adapterRoute(makeAddSurveyController()));
 };
