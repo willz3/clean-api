4268949eac803a18b16f044607755662d05839fa
Willian Vassilek <willz3@outlook.com>
2024-04-02 17:00:05 -0300

    test: ensure AccountMongoRepository allows admin to access any route

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -105,18 +105,53 @@ describe('Account Mongo Repository', () => {
 			expect(account?.password).toBe('any_password');
 		});
 
-		test('Should return an account on loadByToken success with role', async () => {
+		test('Should return an account on loadByToken success with admin role', async () => {
 			await accountCollection.insertOne({
 				name: 'any_name',
 				email: 'any_email@mail.com',
 				password: 'any_password',
 				accessToken: 'any_token',
-				role: 'any_role'
+				role: 'admin'
 			});
 
 			const sut = makeSut();
 
-			const account = await sut.loadByToken('any_token', 'any_role');
+			const account = await sut.loadByToken('any_token', 'admin');
+
+			expect(account).toBeTruthy();
+
+			expect(account?.name).toBe('any_name');
+			expect(account?.email).toBe('any_email@mail.com');
+			expect(account?.password).toBe('any_password');
+		});
+
+		test('Should return null on loadByToken success with invalid role', async () => {
+			await accountCollection.insertOne({
+				name: 'any_name',
+				email: 'any_email@mail.com',
+				password: 'any_password',
+				accessToken: 'any_token'
+			});
+
+			const sut = makeSut();
+
+			const account = await sut.loadByToken('any_token', 'admin');
+
+			expect(account).toBeFalsy();
+		});
+
+		test('Should return an account on loadByToken if user is admin', async () => {
+			await accountCollection.insertOne({
+				name: 'any_name',
+				email: 'any_email@mail.com',
+				password: 'any_password',
+				accessToken: 'any_token',
+				role: 'admin'
+			});
+
+			const sut = makeSut();
+
+			const account = await sut.loadByToken('any_token');
 
 			expect(account).toBeTruthy();
 
--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import app from '../config/app';
 import { Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import env from '../config/env';
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 describe('Survey routes', () => {
 	beforeAll(async () => {
 		await MongoHelper.connect(process.env.MONGO_URL as string);
@@ -15,7 +18,11 @@ describe('Survey routes', () => {
 
 	beforeEach(async () => {
 		surveyCollection = await MongoHelper.getCollection('surveys');
-		await surveyCollection.deleteMany({});
+		accountCollection = await MongoHelper.getCollection('accounts');
+		await Promise.all([
+			surveyCollection.deleteMany({}),
+			accountCollection.deleteMany({})
+		]);
 	});
 
 	describe('POST /surveys', () => {
@@ -36,6 +43,47 @@ describe('Survey routes', () => {
 				});
 
 			expect(httpResponse.statusCode).toBe(403);
+		});
+
+		test('Should return 204 on add survey success with valid token', async () => {
+			const res = await accountCollection.insertOne({
+				name: 'valid_name',
+				email: 'valid_mail@mail.com',
+				password: '123',
+				role: 'admin'
+			});
+
+			const id = res.ops[0]._id;
+			const accessToken = sign({ id }, env.jwtSecret);
+
+			await accountCollection.updateOne(
+				{
+					_id: id
+				},
+				{
+					$set: {
+						accessToken
+					}
+				}
+			);
+
+			const httpResponse = await request(app)
+				.post('/api/surveys')
+				.set('x-access-token', accessToken)
+				.send({
+					question: 'Question',
+					answers: [
+						{
+							answer: 'Answer 1',
+							image: 'http://image-name.com'
+						},
+						{
+							answer: 'Answer 2'
+						}
+					]
+				});
+
+			expect(httpResponse.statusCode).toBe(204);
 		});
 	});
 });
