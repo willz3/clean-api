e4918567ed795248b31792d01646972b91d1e323
Willian Vassilek <willz3@outlook.com>
2024-04-03 15:33:41 -0300

    feat: ensure SurveyMongoRepository load all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,17 @@
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository';
 import {
 	AddSurveyModel,
 	AddSurveyRepository
 } from '../../../../data/usecases/add-survey/db-add-survey-protocols';
+import { SurveyModel } from '../../../../domain/model/survey';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+	async loadAll(): Promise<SurveyModel[]> {
+		const surveyCollection = await MongoHelper.getCollection('surveys');
+		return surveyCollection.find().toArray();
+	}
+
 	async add(survey: AddSurveyModel): Promise<void> {
 		const surveyCollection = await MongoHelper.getCollection('surveys');
 		await surveyCollection.insertOne(survey);
