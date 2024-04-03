66a44e0b42cfe394db47b3901ce68b6ccbebaade
Willian Vassilek <willz3@outlook.com>
2024-04-03 16:26:18 -0300

    feat: load surveys composite route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,11 +3,17 @@ import { adapterRoute } from '../adapters/express/express-routes-adapter';
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory';
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware';
 import { adapterMiddleware } from '../adapters/express/express-middleware-adapter';
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory';
 
 export default (router: Router) => {
 	router.post(
 		'/surveys',
 		adapterMiddleware(makeAuthMiddleware('admin')),
 		adapterRoute(makeAddSurveyController())
+	);
+	router.get(
+		'/surveys',
+		adapterMiddleware(makeAuthMiddleware()),
+		adapterRoute(makeLoadSurveysController())
 	);
 };
