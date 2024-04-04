84e9615d0c4e599063b9e1a22fa3ce44974b0a28
Willian Vassilek <willz3@outlook.com>
2024-04-04 17:29:18 -0300

    chore: mock validator-composite using paths

--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
@@ -1,11 +1,8 @@
-import { Validation } from '../../../../../presentation/protocols/validation';
-import {
-	RequiredFieldValidation,
-	ValidationComposite
-} from '../../../../../validation/validators';
+import { Validation } from '@/presentation/protocols/validation';
+import { RequiredFieldValidation, ValidationComposite } from '@/validation/validators';
 import { makeAddSurveyValidation } from './add-survey-validation-factory';
 
-jest.mock('../../../../../validation/validators/validation-composite');
+jest.mock('@/validation/validators/validation-composite');
 
 describe('AddSurveyValidationFactory', () => {
 	it('Should call ValidationComposite with all validations', () => {
