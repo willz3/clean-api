4fc519d8c91a8c65091b8a7bd6928dc737c647ed
Willian Vassilek <willz3@outlook.com>
2024-04-09 13:46:13 -0300

    feat: ensure SaveSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/suvey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/suvey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -2,3 +2,4 @@ export * from '@/presentation/protocols';
 export * from '@/domain/usecases/survey/load-survey-by-id';
 export * from '@/domain/model/survey';
 export * from '@/presentation/errors';
+export * from '@/presentation/helpers/http/http-helper';
--- src/presentation/controllers/suvey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/suvey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,4 +1,4 @@
-import { forbidden } from '@/presentation/helpers/http/http-helper';
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper';
 import {
 	Controller,
 	HttpRequest,
@@ -11,10 +11,14 @@ export class SaveSurveyResultController implements Controller {
 	constructor(private readonly loadSurveyById: LoadSurveyById) {}
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-		const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId);
-		if (survey == null) {
-			return forbidden(new InvalidParamError('surveyId'));
+		try {
+			const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId);
+			if (survey == null) {
+				return forbidden(new InvalidParamError('surveyId'));
+			}
+			return null;
+		} catch (error) {
+			return serverError(error);
 		}
-		return null;
 	}
 }
