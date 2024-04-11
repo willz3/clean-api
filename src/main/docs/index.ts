b5056798dc32228ad51c370bf486e4ca21cce23d
Willian Vassilek <willz3@outlook.com>
2024-04-11 10:19:04 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginPath } from './paths/login-path';
+import { accountSchema } from './schemas/account-schema';
+import { loginParamsSchema } from './schemas/login-params-schema';
+
 export default {
 	openapi: '3.0.0',
 	info: {
 		title: 'Clean Node API',
 		description: 'A node api created using clean arch and SOLID principles.',
 		version: '1.0.0'
+	},
+	servers: [
+		{
+			url: '/api'
+		}
+	],
+	tags: [
+		{
+			name: 'Login'
+		}
+	],
+	paths: {
+		'/login': loginPath
+	},
+	schemas: {
+		account: accountSchema,
+		loginParams: loginParamsSchema
 	}
 };
