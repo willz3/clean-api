afd0c70dc6a7a511037d701a9cf3cfd1333f9cc0
Willian Vassilek <willz3@outlook.com>
2024-04-11 17:54:31 -0300

    docs: update save result survey result docs

--- src/main/docs/schemas.ts
+++ src/main/docs/schemas.ts
@@ -8,7 +8,8 @@ import {
 	surveysSchema,
 	addSurveyParamsSchema,
 	saveSurveyParamsSchema,
-	surveyResultSchema
+	surveyResultSchema,
+	surveyResultAnswerSchema
 } from './schemas/';
 
 export default {
@@ -21,5 +22,6 @@ export default {
 	addSUrveyParams: addSurveyParamsSchema,
 	signUpParams: signUpParamsSchema,
 	saveSurveyParams: saveSurveyParamsSchema,
-	surveyResult: surveyResultSchema
+	surveyResult: surveyResultSchema,
+	surveyResultAnswer: surveyResultAnswerSchema
 };
