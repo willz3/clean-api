cf65a2c72639a7f6c7d62b9d7d9e7ec040aa9391
Willian Vassilek <willz3@outlook.com>
2024-04-12 13:22:59 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResult with correct value

--- src/presentation/controllers/suvey-result/load-survey-result/load-survey-result-controller-protocols.ts
+++ src/presentation/controllers/suvey-result/load-survey-result/load-survey-result-controller-protocols.ts
@@ -3,3 +3,4 @@ export * from '@/domain/usecases/survey/load-survey-by-id';
 export * from '@/domain/model/survey';
 export * from '@/presentation/errors';
 export * from '@/presentation/helpers/http/http-helper';
+export * from '@/domain/usecases/survey-result/load-survey-result';
--- src/presentation/test/mock-survey-result.ts
+++ src/presentation/test/mock-survey-result.ts
@@ -1,5 +1,6 @@
 import { SurveyResultModel } from '@/domain/model/survey-result';
 import { mockSurveyResultModel } from '@/domain/test';
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result';
 import {
 	SaveSurveyResult,
 	SaveSurveyResultParams
@@ -15,4 +16,13 @@ const mockSaveSurveyResult = (): SaveSurveyResult => {
 	return new SaveSurveyResultStub();
 };
 
-export { mockSaveSurveyResult };
+const mockLoadSurveyResult = (): LoadSurveyResult => {
+	class LoadSurveyResultStub implements LoadSurveyResult {
+		async load(surveyId: string): Promise<SurveyResultModel> {
+			return Promise.resolve(mockSurveyResultModel());
+		}
+	}
+	return new LoadSurveyResultStub();
+};
+
+export { mockSaveSurveyResult, mockLoadSurveyResult };
