3a557e42bb4d699863b403c9d72e8a016bd29513
Willian Vassilek <willz3@outlook.com>
2024-04-12 13:24:08 -0300

    test: ensure LoadSurveyResultController returns 500 if LoadSurveyResult throws

--- src/presentation/controllers/suvey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/suvey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -39,6 +39,13 @@ describe('LoadSurveyResult Controller', () => {
 		expect(loadSpy).toHaveBeenCalledWith('any_id');
 	});
 
+	test('Should return 500 if LoadSurveyResult throws', async () => {
+		const { sut, loadSurveyResultStub } = makeSut();
+		jest.spyOn(loadSurveyResultStub, 'load').mockImplementationOnce(throwError);
+		const httpResponse = await sut.handle(makeFakeRequest());
+		expect(httpResponse).toEqual(serverError(new Error()));
+	});
+
 	const makeFakeRequest = (): HttpRequest => ({
 		params: {
 			surveyId: 'any_id'
