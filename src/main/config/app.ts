62a496440ed7cc13a3a5d63be0e235efdd622e40
Willian Vassilek <willz3@outlook.com>
2024-04-12 17:11:21 -0300

    feat: add route to serve static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import setupMiddlewares from './middlewares';
 import setupRoutes from './routes';
 import setupSwagger from './swagger';
+import setupStaticFiles from './static-files';
 
 const app = express();
 setupSwagger(app);
 setupMiddlewares(app);
 setupRoutes(app);
+setupStaticFiles(app);
 
 export default app;
---
+++ src/main/config/static-files.ts
@@ -0,0 +1,7 @@
+/* eslint-disable n/no-path-concat */
+import express, { Express } from 'express';
+import path from 'path';
+
+export default (app: Express): void => {
+	app.use('/static', express.static(path.resolve(__dirname, '../../static')));
+};
