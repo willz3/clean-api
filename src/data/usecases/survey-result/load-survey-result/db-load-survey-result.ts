061b277663497b0b9c3d94cd3fb42a968b135a64
Willian Vassilek <willz3@outlook.com>
2024-04-12 11:17:38 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct value

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,13 @@
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result';
+
+import { SurveyResultModel } from '@/domain/model/survey-result';
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+	constructor(private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+
+	async load(surveyId: string): Promise<SurveyResultModel> {
+		await this.loadSurveyResultRepository.loadBySurveyId(surveyId);
+		return null;
+	}
+}
