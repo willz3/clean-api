6e162c0268eda7cf9364165c3cd100cb3cba5b75
Willian Vassilek <willz3@outlook.com>
2024-04-12 13:38:10 -0300

    docs: add load survey result api docs

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -47,5 +47,47 @@ export const surveyResultPath = {
 				$ref: '#/components/serverError'
 			}
 		}
+	},
+	get: {
+		security: [
+			{
+				apiKeyAuth: []
+			}
+		],
+		tags: ['Survey'],
+		summary: 'API to consult a result of a survey',
+		description: 'This route only can be access by authenticate users.',
+		parameters: [
+			{
+				in: 'path',
+				name: 'surveyId',
+				description: 'Survey of id that has been answered',
+				required: true,
+				schema: {
+					type: 'string'
+				}
+			}
+		],
+		responses: {
+			200: {
+				description: 'Success',
+				content: {
+					'application/json': {
+						schema: {
+							$ref: '#/schemas/surveyResult'
+						}
+					}
+				}
+			},
+			403: {
+				$ref: '#/components/forbidden'
+			},
+			404: {
+				$ref: '#/components/notFound'
+			},
+			500: {
+				$ref: '#/components/serverError'
+			}
+		}
 	}
 };
