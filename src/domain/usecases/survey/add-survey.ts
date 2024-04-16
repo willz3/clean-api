98389b7fec5142f950b306283e9d45da7242b06d
Willian Vassilek <willz3@outlook.com>
2024-04-16 13:08:56 -0300

    refacotor: use omit in add survey use case protocol

--- src/domain/usecases/survey/add-survey.ts
+++ src/domain/usecases/survey/add-survey.ts
@@ -1,15 +1,11 @@
-import { SurveysAnswerModel } from '@/domain/model/survey';
+import { SurveyModel } from '@/domain/model/survey';
 
 export interface AddSurvey {
 	add(account: AddSurvey.Params): Promise<AddSurvey.Result>;
 }
 
 export namespace AddSurvey {
-	export type Params = {
-		question: string;
-		answers: SurveysAnswerModel[];
-		date: Date;
-		didAnswer?: boolean;
-	};
+	export type Params = Omit<SurveyModel, 'id'>;
+
 	export type Result = void;
 }
