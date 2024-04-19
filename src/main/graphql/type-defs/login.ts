dc689dd530d39685fa64f22750c42e13439e197f
Willian Vassilek <willz3@outlook.com>
2024-04-19 11:41:37 -0300

    feat: add signup mutation

--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -3,6 +3,11 @@ import { gql } from 'apollo-server-express';
 export default gql`
 	extend type Query {
 		login(email: String!, password: String!): Account!
+		signUp(name: String!, email: String!, password: String!): Account!
+	}
+
+	extend type Mutation {
+		signUp(name: String!, email: String!, password: String!): Account!
 	}
 
 	type Account: {
